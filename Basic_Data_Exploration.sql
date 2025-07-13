80c80259c1b69d9e15a6adf49b059b01d7e8b368
Goro25 <gauravsalgaonkar878@gmail.com>
2025-07-13 06:12:17 +0530

    Basic aggregate functions count,min,max,average

---
+++ Basic_Data_Exploration.sql
@@ -0,0 +1,2 @@
+Select Count(*) as Total_Orders,Min(Delivery_Time_min) as Min_Delivery_Time,Max(Delivery_Time_min) as Max_Delivery_Time,Avg(Delivery_Time_min) as Average_Delivery_Time
+From Food_Delivery
